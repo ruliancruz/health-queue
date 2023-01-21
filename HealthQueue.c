829b40c3585c1b9ca80ae6a90e0b4ed10d1b3456
Gabriel Cavalcante de Jesus Oliveira <94535032+gacav-lab@users.noreply.github.com>
2023-01-21 08:58:15 -0300

    :recycle: refactor: pequenas alterações

--- HealthQueue.c
+++ HealthQueue.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
-#define LENGTH 101
+#define BUF_SIZE 101
 
 // Tudo que tem inicial "L", se refere a lista (list)
 // Tudo que tem inicial "Q", se refere a fila (queue)
@@ -14,45 +14,39 @@ int lastIdPatient = 0;
 typedef struct LCellule *LPointer;
 typedef struct QCellule *QPointer;
 
-typedef struct
-{
+typedef struct {
 	int id;
-	char rg[LENGTH];
-	char name[LENGTH];
-	char address[LENGTH];
+	char rg[BUF_SIZE];
+	char name[BUF_SIZE];
+	char address[BUF_SIZE];
 	int age;
-	char risk[LENGTH];
+	char risk[BUF_SIZE];
 } Patient;
 
-typedef struct
-{
+typedef struct {
 	QPointer front;
 	QPointer back;
 } Queue;
 
-typedef struct
-{
+typedef struct {
 	int id;
-	char cnpj[LENGTH];
-	char name[LENGTH];
-	char address[LENGTH];
+	char cnpj[BUF_SIZE];
+	char name[BUF_SIZE];
+	char address[BUF_SIZE];
 	Queue patientQueue;
 } Hospital;
 
-typedef struct LCellule
-{
+typedef struct LCellule {
 	Hospital item;
 	LPointer next;
 } LCellule;
 
-typedef struct QCellule
-{
+typedef struct QCellule {
 	Patient item;
 	QPointer next;
 } QCellule;
 
-typedef struct
-{
+typedef struct {
 	LPointer first, last;
 } List;
 
@@ -68,15 +62,14 @@ void printList(List list);
 void printQueue(Queue queue, int idHospital);
 Hospital FSearchHospital(List list, int id); // Procura hospital com esse id na lista
 void userMenu(void); // "void function(void)" eh diferente de "void function()"
+// "F" de "function", como prefixo, para evitar conflitos com as constantes da enumeracao
 Hospital FRegisterHospital(Hospital *Hospital);
 Patient FRegisterPatient(Patient *patient);
-// "F" de "function", como prefixo, para evitar conflitos com as constantes da enumeracao
 void FShowHospitals(void);
 void FShowPatients(void);
 void FAttendPatient(Hospital *hospital, Patient *item);
 
-enum USER_MENU
-{
+enum USER_MENU {
 	registerHospital = 1,
 	registerPatient,
 	showHospitals,
@@ -85,8 +78,7 @@ enum USER_MENU
 	quit
 } option;
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 	List list;
 	Queue queue;
 	Patient patient;
@@ -98,18 +90,16 @@ int main(int argc, char **argv)
 	makeEmptyList(&list);
 	makeEmptyQueue(&queue);
 
-	do
-	{
+	do {
     	userMenu();
+
     	printf("Opcao: ");
     	scanf("%d", &auxiliary);
 
-		// Armazenar na variavel "option" diretamente pelo "scanf", gera um aviso
-    	option = auxiliary;
-    	puts("");
+    	option = auxiliary; // Armazenar em "option" diretamente, gera um aviso
+    	printf("\n");
 
-    	switch(option)
-    	{
+    	switch(option) {
         	case registerHospital:
             	fflush(stdin);
             	hospital = FRegisterHospital(&hospital);
@@ -118,45 +108,40 @@ int main(int argc, char **argv)
         	case registerPatient:
             	fflush(stdin);
 
-            	if(LIsEmpty(list))
+            	if(LIsEmpty(list)) {
                 	puts("Nao existem hospitais cadastrados.\n\n");
-            	else
-            	{
+				} else {
                 	printList(list);
+
                 	printf("Digite o id do hospital no qual deseja cadastrar o paciente: ");
                 	scanf("%d", &auxiliary);
 
                 	Aux = list.first->next;
 
-                	while (Aux != NULL)
-                	{
-                    	if(Aux->item.id >= auxiliary)
-                    	{
-                        	if(Aux->item.id == auxiliary)
-                        	{
+                	while (Aux != NULL) {
+                    	if(Aux->item.id >= auxiliary) {
+                        	if(Aux->item.id == auxiliary) {
                             	hospitalPointer = &Aux->item;
                             	break;
-                        	}
-                        	else
-                        	{
+                        	} else {
                             	auxiliary = -1;
                             	break;
                         	}
-                    	}
-                    	else
+                    	} else {
                         	Aux = Aux->next;
+						}
                 	}
 
-                	if(Aux == NULL)
+                	if(Aux == NULL) {
                     	auxiliary = -1;
+					}
 
-                	if(auxiliary != -1)
-                	{
+                	if(auxiliary != -1) {
                     	patient = FRegisterPatient(&patient);
                     	enqueueItem(&hospitalPointer->patientQueue, patient);
-                	}
-                	else
+                	} else {
                     	puts("\nNao existe nenhum hospital com esse Id.\n");
+					}
             	}
 
             	break;
@@ -164,65 +149,63 @@ int main(int argc, char **argv)
             	printList(list);
             	break;
         	case showPatients:
-        		if(LIsEmpty(list))
+        		if(LIsEmpty(list)) {
                 	puts("Nao existem hospitais e nem pacientes cadastrados.\n\n");
-            	else
-            	{
+				} else {
             		printList(list);
+
             		printf("Digite o Id do hospital o qual deseja consultar a fila de pacientes: ");
             		scanf("%d", &auxiliary);
+
             		hospital = FSearchHospital(list, auxiliary);
+
             		printQueue(hospital.patientQueue, hospital.id);
             	}
 
             	break;
         	case attendPatient:
-        		if(LIsEmpty(list))
+        		if(LIsEmpty(list)) {
                 	puts("Nao existem hospitais e nem pacientes cadastrados.\n\n");
-            	else
-            	{
+				} else {
             		fflush(stdin);
             		printList(list);
+
                 	printf("Digite o id do hospital no qual deseja atender um paciente da fila: ");
                 	scanf("%d", &auxiliary);
 
                 	Aux = list.first->next;
 
-                	while (Aux != NULL)
-                	{
-                    	if(Aux->item.id >= auxiliary)
-                    	{
-                        	if(Aux->item.id == auxiliary)
-                        	{
+                	while (Aux != NULL) {
+                    	if(Aux->item.id >= auxiliary) {
+                        	if(Aux->item.id == auxiliary) {
                             	hospitalPointer = &Aux->item;
                             	break;
-                        	}
-                        	else
-                        	{
+                        	} else {
                             	auxiliary = -1;
                             	break;
                         	}
-                    	}
-                    	else
+                    	} else {
                         	Aux = Aux->next;
+						}
                 	}
 
                 	if(Aux == NULL) {
                     	auxiliary = -1;
 					}
 
-                	if(auxiliary != -1)
-                	{
+                	if(auxiliary != -1) {
                     	dequeueItem(&hospitalPointer->patientQueue, &hospitalPointer->patientQueue.front->next->item);
-                	}
-                	else
+                	} else {
                     	puts("\nNao existe nenhum hospital com esse id.\n");
+					}
             	}
+
             	break;
         	case quit:
-            	puts("Saindo...");
+				printf("Saindo...\n");
             	sleep(1);
             	system("cls");
+
             	return EXIT_SUCCESS;
         	default:
 				system("cls");
@@ -234,37 +217,32 @@ int main(int argc, char **argv)
 	return EXIT_SUCCESS;
 }
 
-void makeEmptyList(List *list)
-{
+void makeEmptyList(List *list) {
 	/* Existe mais uma celula, para evitar comparacoes na insercao, aumentando a performance */
 	list->first = (LPointer) malloc(sizeof(LCellule));
 	list->last = list->first;
 	list->first->next = NULL;
 }
 
-void makeEmptyQueue(Queue *queue)
-{
+void makeEmptyQueue(Queue *queue) {
 	/* Existe mais uma celula, para evitar comparacoes no enfileiramento, aumentando a performance */
 	queue->front = (QPointer) malloc(sizeof(QCellule));
 	queue->back = queue->front;
 	queue->front->next = NULL;
 }
 
-void insertItem(List *list, Hospital item)
-{
+void insertItem(List *list, Hospital item) {
 	list->last->next = (LPointer) malloc(sizeof(LCellule));
 	list->last = list->last->next;
 	list->last->item = item;
 	list->last->next = NULL;
 }
 
-void removeItem(LPointer auxiliary, List *list, Hospital *item)
-{
+void removeItem(LPointer auxiliary, List *list, Hospital *item) {
 	// O item a ser removido eh o seguinte ao apontado por "auxiliary"
 	LPointer target;
 
-	if(LIsEmpty(*list) || auxiliary == NULL || auxiliary->next == NULL)
-	{
+	if(LIsEmpty(*list) || auxiliary == NULL || auxiliary->next == NULL) {
     	puts("Erro: lista vazia ou posicao nao existe");
     	return; // so para sair da funcao
 	}
@@ -273,14 +251,14 @@ void removeItem(LPointer auxiliary, List *list, Hospital *item)
 	*item = target->item;
 	auxiliary->next = target->next;
 
-	if(auxiliary->next == NULL)
+	if(auxiliary->next == NULL) {
     	list->last = auxiliary;
+	}
 
 	free(target);
 }
 
-void enqueueItem(Queue *queue, Patient item)
-{
+void enqueueItem(Queue *queue, Patient item) {
 	QPointer Aux;
 
 	queue->back->next = (QPointer) malloc(sizeof(QCellule));
@@ -289,12 +267,10 @@ void enqueueItem(Queue *queue, Patient item)
 	queue->back->next = NULL;
 }
 
-void dequeueItem(Queue *queue, Patient *item)
-{
+void dequeueItem(Queue *queue, Patient *item) {
 	QPointer target;
 
-	if (QIsEmpty(*queue))
-	{
+	if (QIsEmpty(*queue)) {
     	puts("Erro: fila vazia");
     	return;
 	}
@@ -305,24 +281,20 @@ void dequeueItem(Queue *queue, Patient *item)
 	free(target);
 }
 
-bool LIsEmpty(List list)
-{
+bool LIsEmpty(List list) {
 	return (list.first == list.last);
 }
 
-bool QIsEmpty(Queue queue)
-{
+bool QIsEmpty(Queue queue) {
 	return (queue.front == queue.back);
 }
 
-void printList(List list)
-{
+void printList(List list) {
 	puts("Hospitais Cadastrados\n");
 	LPointer Aux;
 	Aux = list.first->next;
 
-	while (Aux != NULL)
-	{
+	while (Aux != NULL) {
     	printf("Id %d\n", Aux->item.id);
     	printf("Nome: %s", Aux->item.name);
     	printf("Endereco: %s", Aux->item.address);
@@ -331,15 +303,13 @@ void printList(List list)
 	}
 }
 
-void printQueue(Queue queue, int idHospital)
-{
+void printQueue(Queue queue, int idHospital) {
 	QPointer Aux;
 	Aux = queue.front->next;
 	int counter = 0;
 	printf("\nPacientes do Hospital %d\n\n", idHospital);
 
-	while (Aux != NULL)
-	{
+	while (Aux != NULL) {
     	printf("Paciente %d\n", counter);
     	printf("Nome: %s", Aux->item.name);
     	printf("Idade: %d\n", Aux->item.age);
@@ -350,29 +320,27 @@ void printQueue(Queue queue, int idHospital)
     	counter++;
 	}
 
-	puts("");
+	printf("\n");
 }
 
 
-void userMenu()
-{
-	puts("O que voce deseja fazer?\n\n"\
-     	"\t[1] - Cadastrar hospital\n"\
-     	"\t[2] - Cadastrar paciente\n"\
-     	"\t[3] - Mostrar hospitais\n"\
-     	"\t[4] - Mostrar pacientes\n"\
-     	"\t[5] - Atender paciente\n"\
-     	"\t[6] - Sair\n");
+void userMenu(void) {
+	printf("O que voce deseja fazer?\n\n"\
+     	   "\t[1] - Cadastrar hospital\n"\
+     	   "\t[2] - Cadastrar paciente\n"\
+     	   "\t[3] - Mostrar hospitais\n"\
+     	   "\t[4] - Mostrar pacientes\n"\
+     	   "\t[5] - Atender paciente\n"\
+     	   "\t[6] - Sair\n\n");
 }
 
-Patient FRegisterPatient(Patient *patient)
-{
+Patient FRegisterPatient(Patient *patient) {
 	patient->id = lastId;
 	lastId++;
 
 	fflush(stdin);
 	printf("Nome: ");
-	fgets(patient->name, LENGTH, stdin);
+	fgets(patient->name, BUF_SIZE, stdin);
 	// Remove o caractere de nova linha "\n" armazenado com a string
 	length = strlen(patient->name);
 	patient->name[length] = '\0';
@@ -382,19 +350,19 @@ Patient FRegisterPatient(Patient *patient)
 
 	fflush(stdin);
 	printf("Endereco: ");
-	fgets(patient->address, LENGTH, stdin);
+	fgets(patient->address, BUF_SIZE, stdin);
 	length = strlen(patient->address);
 	patient->address[length] = '\0';
 
 	fflush(stdin);
 	printf("Risco: ");
-	fgets(patient->risk, LENGTH, stdin);
+	fgets(patient->risk, BUF_SIZE, stdin);
 	length = strlen(patient->risk);
 	patient->risk[length] = '\0';
 
 	fflush(stdin);
 	printf("RG: ");
-	fgets(patient->rg, LENGTH, stdin);
+	fgets(patient->rg, BUF_SIZE, stdin);
 	length = strlen(patient->rg);
 	patient->rg[length] = '\0';
 	puts("");
@@ -402,73 +370,68 @@ Patient FRegisterPatient(Patient *patient)
 	return *patient;
 }
 
-Hospital FSearchHospital(List list, int id)
-{
+Hospital FSearchHospital(List list, int id) {
 	LPointer Aux;
 	Hospital hospital;
 
 	Aux = list.first->next;
 	hospital.id = -1;
 
-	while (Aux != NULL)
-	{
-    	if(Aux->item.id >= id)
-    	{
-        	if(Aux->item.id == id)
+	while (Aux != NULL) {
+    	if(Aux->item.id >= id) {
+        	if(Aux->item.id == id) {
             	return Aux->item;
-        	else
+			} else {
             	return hospital;
-    	}
-    	else
+			}
+    	} else {
         	Aux = Aux->next;
+		}
 	}
 
 	return hospital;
 }
 
-Patient FSearchPatient(Hospital hospital, int id)
-{
+Patient FSearchPatient(Hospital hospital, int id) {
 	QPointer Aux;
 	Patient patient;
 
 	Aux = hospital.patientQueue.front->next;
 	patient.id = -1;
 
-	while (Aux != NULL)
-	{
-    	if(Aux->item.id >= id)
-    	{
-        	if(Aux->item.id == id)
+	while (Aux != NULL) {
+    	if(Aux->item.id >= id) {
+        	if(Aux->item.id == id) {
             	return Aux->item;
-        	else
+			} else {
             	return patient;
-    	}
-    	else
+			}
+    	} else {
         	Aux = Aux->next;
+		}
 	}
 
 	return patient;
 }
 
-Hospital FRegisterHospital(Hospital *hospital)
-{
+Hospital FRegisterHospital(Hospital *hospital) {
 	hospital->id = lastIdPatient;
 	lastIdPatient++;
 
 	makeEmptyQueue(&hospital->patientQueue);
 
 	printf("Nome: ");
-	fgets(hospital->name, LENGTH, stdin);
+	fgets(hospital->name, BUF_SIZE, stdin);
 	length = strlen(hospital->name);
 	hospital->name[length] = '\0';
 
 	printf("Endereco: ");
-	fgets(hospital->address, LENGTH, stdin);
+	fgets(hospital->address, BUF_SIZE, stdin);
 	length = strlen(hospital->address);
 	hospital->address[length] = '\0';
 
 	printf("CNPJ: ");
-	fgets(hospital->cnpj, LENGTH, stdin);
+	fgets(hospital->cnpj, BUF_SIZE, stdin);
 	length = strlen(hospital->cnpj);
 	hospital->cnpj[length] = '\0';
 	puts("");
